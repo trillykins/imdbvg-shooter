251e08fa15e47cd897f4d8081326e18e19556fec
Kagemand Andersen <trillykins@gmail.com>
2019-05-20 19:56:50 +0200

    Fixed respawn issue + level pool

--- src/assets/ChooseLevel.cs
+++ src/assets/ChooseLevel.cs
@@ -1,23 +1,28 @@
 ﻿using UnityEngine;
 using System.Linq;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ChooseLevel : MonoBehaviour
 {
     public GameObject[] levels;
+    private List<GameObject> levelPool = new List<GameObject>();
 
     void Awake()
     {
-        Instantiate(levels[Random.Range(0, levels.Length)], transform.position, transform.rotation);
+        foreach (var level in levels)
+        {
+            var l = Instantiate(level, transform.position, transform.rotation);
+            l.SetActive(false);
+            levelPool.Add(l);
+        }
+        levelPool[Random.Range(0, levels.Length)].SetActive(true);
     }
 
     public void GetNewLevel()
     {
-        foreach (var gameObject in GameObject.FindGameObjectsWithTag("Level"))
-        {
-            Destroy(gameObject);
-        }
-
-        Instantiate(levels[Random.Range(0, levels.Length)], transform.position, transform.rotation);
+        levelPool.ForEach(x => x.SetActive(false));
+        var level = levelPool.ElementAt(Random.Range(0, levelPool.Count()));
+        level.SetActive(true);
     }
 }
